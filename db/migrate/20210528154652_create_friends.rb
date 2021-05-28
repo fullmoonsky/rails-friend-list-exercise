a98b0e6ef984f7fce243f0ec3f5d6ba1aaaa02d2
Hong Staley <hngstaley@gmail.com>
2021-05-28 15:12:42 -0400

    initial commit

---
+++ db/migrate/20210528154652_create_friends.rb
@@ -0,0 +1,14 @@
+class CreateFriends < ActiveRecord::Migration[6.1]
+  def change
+    create_table :friends do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :mobile_phone
+      t.string :home_phone
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,16 @@
+
+
+ActiveRecord::Schema.define(version: 2021_05_28_154652) do
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "mobile_phone"
+    t.string "home_phone"
+    t.text "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
